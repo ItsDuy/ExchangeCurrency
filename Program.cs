7914b22c6b88af77f0b5e2235028aff75be1862d
ItsDuy <duy.nguyen280805@hcmut.edu.vn>
2024-08-06 21:23:58 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exhangecurrency;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double Rate=23000;
+        double USD,VND;
+        Console.WriteLine("Enter the amount of USD you want to exhange into VND");
+        double.TryParse(Console.ReadLine(), out USD);
+        VND=Rate*USD;
+        Console.WriteLine($"You received {VND} dong");
+    }
+}
